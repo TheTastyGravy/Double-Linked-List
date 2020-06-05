a0d6cd390446e65eaaf269d235740e8b32056cad
TheTastyGravy <cnpowerman23@gmail.com>
2020-06-05 13:12:07 +1000

    Add textbox class

---
+++ Project1/TextboxClass.cpp
@@ -0,0 +1,60 @@
+#include "TextboxClass.h"
+
+TextboxClass::TextboxClass(Rectangle bounds, const int MAX_SIZE, int fontSize) :
+	bounds(bounds),
+	text(""),
+	MAX_SIZE(MAX_SIZE),
+	fontSize(fontSize)
+{
+}
+
+
+void TextboxClass::setBounds(Rectangle bounds)
+{
+	this->bounds = bounds;
+}
+
+
+void TextboxClass::update()
+{
+	int key = GetKeyPressed();
+
+	// 0 is a null terminator
+	while (key > 0)
+	{
+		// Keys within this range are printable
+		if ((key >= 32) && (key <= 125) && (text.length() < MAX_SIZE))
+		{
+			// In this case, we only want ints
+			if ((key >= 48) && (key <= 57))
+			{
+				text += (char)key;
+			}
+		}
+
+		// Get next key in queue
+		key = GetKeyPressed();
+	}
+
+	if (IsKeyPressed(KEY_BACKSPACE))
+		text.pop_back();
+
+}
+
+std::string TextboxClass::getTextAndClear()
+{
+	// Return text after clearing it
+	std::string temp = text;
+	text = "";
+	return temp;
+}
+
+
+void TextboxClass::draw()
+{
+	// Draw white rectangle with black outline
+	DrawRectangleRec(bounds, WHITE);
+	DrawRectangleLinesEx(bounds, 2, BLACK);
+	// Draw the current text
+	DrawText(text.c_str(), bounds.x + 5, bounds.y + bounds.height / 2 - fontSize / 2, fontSize, BLACK);
+}
---
+++ Project1/TextboxClass.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "raylib.h"
+#include <string>
+
+class TextboxClass
+{
+public:
+	TextboxClass(Rectangle bounds, const int MAX_SIZE, int fontSize = 20);
+
+
+	// Set the position and size of the textbox
+	void setBounds(Rectangle bounds);
+
+	// Update the current text using keys pressed
+	void update();
+	// Get the current text, and clear it
+	std::string getTextAndClear();
+
+	// Draw the textbox. BeginDrawing() must have been called to do so
+	void draw();
+
+private:
+	Rectangle bounds;
+	std::string text;
+	const int MAX_SIZE;
+	int fontSize;
+};

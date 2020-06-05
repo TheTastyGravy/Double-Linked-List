008c517c63193198dc025de725a3e7789296a389
TheTastyGravy <cnpowerman23@gmail.com>
2020-06-05 12:12:32 +1000

    Add button class

---
+++ Project1/ButtonClass.cpp
@@ -0,0 +1,51 @@
+#include "ButtonClass.h"
+
+ButtonClass::ButtonClass(Rectangle bounds, std::string text) :
+	bounds(bounds),
+	text(text),
+	fontSize(20),
+	normalColor(RAYWHITE),
+	mouseOverColor(GRAY)
+{
+}
+
+
+void ButtonClass::setBounds(Rectangle bounds)
+{
+	this->bounds = bounds;
+}
+
+void ButtonClass::setText(std::string text, int fontSize)
+{
+	this->text = text;
+	this->fontSize = fontSize;
+}
+
+void ButtonClass::setColor(Color normal, Color mouseOver)
+{
+	normalColor = normal;
+	mouseOverColor = mouseOver;
+}
+
+
+bool ButtonClass::isMouseOver()
+{
+	return (CheckCollisionPointRec(GetMousePosition(), bounds));
+}
+
+bool ButtonClass::isClicked()
+{
+	return (this->isMouseOver() && IsMouseButtonPressed(MOUSE_LEFT_BUTTON));
+}
+
+
+void ButtonClass::draw()
+{
+	// Draw the box using the appropriate color and with an outline
+	DrawRectangleRec(bounds, (isMouseOver()) ? mouseOverColor : normalColor);
+	DrawRectangleLinesEx(bounds, 2, BLACK);
+	
+	// Draw the text inside the box
+	DrawText(text.c_str(), bounds.x + bounds.width/2 - MeasureText(text.c_str(), fontSize)/2, bounds.y + bounds.height/2 - fontSize/2, fontSize, BLACK);
+	// bounds.x + bounds.width/2 - MeasureText(...)/2 is used to center the text
+}
---
+++ Project1/ButtonClass.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "raylib.h"
+#include <string>
+
+// Custom button class using raylib
+class ButtonClass
+{
+public:
+	ButtonClass(Rectangle bounds, std::string text);
+
+
+	// Set the position and size of the button
+	void setBounds(Rectangle bounds);
+	// Set the buttons displayed text
+	void setText(std::string text, int fontSize);
+	// Set the buttons color when the mouse is not over the button (normal),
+	// and when is is within its bounds (mouseOver)
+	void setColor(Color normal, Color mouseOver);
+
+	// Is the mouse within the buttons bounds?
+	bool isMouseOver();
+	// Has the button been clicked?
+	bool isClicked();
+
+	// Draw the buton. BeginDrawing() must have been called to do so
+	void draw();
+
+private:
+	Rectangle bounds;
+	std::string text;
+	int fontSize;
+	Color normalColor;
+	Color mouseOverColor;
+};
